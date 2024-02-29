5b970ef480953d7ba55c005ff8783c042f3760ff
EFFemole <go.netgle@gmail.com>
2024-02-29 18:50:19 +0200

    feat: provided post list

--- app/src/main/java/com/posthub/MainActivity.kt
+++ app/src/main/java/com/posthub/MainActivity.kt
@@ -1,33 +1,25 @@
 package com.posthub
 
 import android.os.Bundle
-import android.util.Log
+import android.text.Html
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.posthub.ui.theme.PostHubTheme
-import com.posthub.viewModels.MainViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import io.github.jan.supabase.SupabaseClient
-import io.github.jan.supabase.createSupabaseClient
-import io.github.jan.supabase.postgrest.Postgrest
-import io.github.jan.supabase.postgrest.from
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.Serializable
-import java.sql.Timestamp
-
-var supabase: SupabaseClient = createSupabaseClient(
-    supabaseUrl = "https://uqgggijqjezqffbgytie.supabase.co",
-    supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxZ2dnaWpxamV6cWZmYmd5dGllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA0Nzk4MTcsImV4cCI6MjAxNjA1NTgxN30.48KSE1m_uk68kecgbDQM2F9BjJ9FVczSWJYTfPz-OAs"
-) {
-    install(Postgrest)
-}
+import com.posthub.viewModels.PostsViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,28 +35,40 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Serializable
-data class Post (
-    val id: String?,
-    val created_at: String?,
-    val updated_at: String?,
-    val author_id: String?,
-    val title: String?,
-    val content: String?,
-    val count_view: Int?,
-    val image_path: String?,
-    val age_rating_id: Int?,
-)
-
 @Composable
-fun PostList() {
-    LaunchedEffect(Unit) {
-        withContext(Dispatchers.IO) {
-            val result = supabase.from("posts").select().decodeList<Post>()
-            for (post in result) {
-                Log.d("supabase", post.title.toString())
+fun PostList(viewModel: PostsViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
+    LazyColumn(contentPadding = PaddingValues(20.dp), verticalArrangement = Arrangement.spacedBy(10.dp)) {
+        items(viewModel.states.value.posts) { post ->
+            Card() {
+                Column(Modifier.padding(20.dp)) {
+                    post.title?.let {
+                        Text(
+                            text = it,
+                            style = MaterialTheme.typography.headlineSmall,
+                            maxLines = 3,
+                            overflow = TextOverflow.Ellipsis,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    post.content?.let { text ->
+//                        Text(
+//                            text = Html.fromHtml(text, Html.FROM_HTML_MODE_LEGACY),
+//                            maxLines = 5,
+//                            overflow = TextOverflow.Ellipsis
+//                        )
+                        AndroidView(
+                            factory = {
+                                TextView(it)
+                            },
+                            update = {
+                                it.text = Html.fromHtml(text)
+                            }
+                        )
+                    }
+
+                }
             }
         }
-
     }
 }
